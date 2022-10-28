d77000e998c2de68ed67de16647603831d963404
nozomu-karai <karai.nozomu.77c@st.kyoto-u.ac.jp>
2022-10-28 13:47:37 +0900

    物体検知の結果をキャッシュできるようにした

---
+++ cache_info.py
@@ -0,0 +1,146 @@
+# Copyright (c) Facebook, Inc. and its affiliates.
+import argparse
+import glob
+import json
+import multiprocessing as mp
+import numpy as np
+import os
+import random
+import tempfile
+import time
+import warnings
+import cv2
+from regex import F
+import tqdm
+import sys
+import mss
+
+from detectron2.config import get_cfg
+from detectron2.data.detection_utils import read_image
+from detectron2.utils.logger import setup_logger
+
+sys.path.insert(0, 'third_party/CenterNet2/')
+from centernet.config import add_centernet_config
+from detic.config import add_detic_config
+
+from detic.predictor import VisualizationDemo
+
+
+# constants
+WINDOW_NAME = "Detic"
+
+def setup_cfg(args):
+    cfg = get_cfg()
+    if args.cpu:
+        cfg.MODEL.DEVICE="cpu"
+    add_centernet_config(cfg)
+    add_detic_config(cfg)
+    cfg.merge_from_file(args.config_file)
+    cfg.merge_from_list(args.opts)
+    # Set score_threshold for builtin models
+    cfg.MODEL.RETINANET.SCORE_THRESH_TEST = args.confidence_threshold
+    cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = args.confidence_threshold
+    cfg.MODEL.PANOPTIC_FPN.COMBINE.INSTANCES_CONFIDENCE_THRESH = args.confidence_threshold
+    cfg.MODEL.ROI_BOX_HEAD.ZEROSHOT_WEIGHT_PATH = 'rand' # load later
+    if not args.pred_all_class:
+        cfg.MODEL.ROI_HEADS.ONE_CLASS_PER_PROPOSAL = True
+    cfg.freeze()
+    return cfg
+
+
+def get_parser():
+    parser = argparse.ArgumentParser(description="Detectron2 demo for builtin configs")
+    parser.add_argument(
+        "--config-file",
+        default="configs/quick_schedules/mask_rcnn_R_50_FPN_inference_acc_test.yaml",
+        metavar="FILE",
+        help="path to config file",
+    )
+    parser.add_argument("--cpu", action='store_true', help="Use CPU only.")
+    parser.add_argument(
+        "--input",
+        nargs="+",
+        help="A list of space separated input images; "
+        "or a single glob pattern such as 'directory/*.jpg'",
+    )
+    parser.add_argument(
+        "--output",
+        help="A file or directory to save output visualizations. "
+        "If not given, will show output in an OpenCV window.",
+    )
+    parser.add_argument(
+        "--vocabulary",
+        default="lvis",
+        choices=['lvis', 'openimages', 'objects365', 'coco', 'custom'],
+        help="",
+    )
+    parser.add_argument(
+        "--custom_vocabulary",
+        default="",
+        help="",
+    )
+    parser.add_argument("--pred_all_class", action='store_true')
+    parser.add_argument(
+        "--confidence-threshold",
+        type=float,
+        default=0.5,
+        help="Minimum score for instance predictions to be shown",
+    )
+    parser.add_argument(
+        "--opts",
+        help="Modify config options using the command-line 'KEY VALUE' pairs",
+        default=[],
+        nargs=argparse.REMAINDER,
+    )
+    return parser
+
+
+if __name__ == "__main__":
+    mp.set_start_method("spawn", force=True)
+    args = get_parser().parse_args()
+    inputs = []
+    with open(args.input[0], 'r') as f:
+        for line in f:
+            line = line.strip()
+            inputs.append('/home/karai/dataset/flickr30k_entities/flickr30k-images/' + line + '.jpg')
+    setup_logger(name="fvcore")
+    logger = setup_logger()
+    logger.info("Arguments: " + str(args))
+    save_file = open(os.path.join(args.output, 'cache_data.json'), 'a')
+    cache = {}
+
+    cfg = setup_cfg(args)
+
+    demo = VisualizationDemo(cfg, args)
+
+    if args.input:
+        if len(args.input) == 1:
+            args.input = glob.glob(os.path.expanduser(args.input[0]))
+            assert args.input, "The input path(s) was not found"
+        for path in tqdm.tqdm(inputs, disable=not args.output):
+            img = read_image(path, format="BGR")
+            start_time = time.time()
+            predictions, visualized_output = demo.run_on_image(img)
+            # logger.info(
+            #     "{}: {} in {:.2f}s".format(
+            #         path,
+            #         "detected {} instances".format(len(predictions["instances"]))
+            #         if "instances" in predictions
+            #         else "finished",
+            #         time.time() - start_time,
+            #     )
+            # )
+            predictions = predictions['instances']
+            cache[os.path.basename(path)] = {}
+            cache[os.path.basename(path)]['classes'] = [demo.metadata.thing_classes[x] for x in predictions.pred_classes.tolist()]
+            cache[os.path.basename(path)]['scores'] = predictions.scores.tolist()
+            cache[os.path.basename(path)]['boxes'] = predictions.pred_boxes.tensor.tolist()
+
+            if args.output:
+                out_filename = os.path.join(args.output, os.path.basename(path))
+                th = random.random()
+                if th < 0.05:
+                    visualized_output.save(out_filename)
+    
+    save_file.write(json.dumps(cache, indent=4))
+    save_file.close()
